f9d66faad0e0edd5d22db2afd94856dce24edf98
rehan <mohdrehanali40@gmail.com>
2025-02-26 10:40:37 +0530

    feat: added the checbox and textfield theme in theme file

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -16,6 +16,39 @@ const theme = createTheme({
   typography: {
     fontFamily: "'Luckiest Guy', cursive",
   },
+  components: {
+    MuiTextField: {
+      defaultProps: {
+        variant: "outlined", // Ensure consistent variant
+      },
+      styleOverrides: {
+        root: {
+          "& .MuiOutlinedInput-root": {
+            "&.Mui-focused": {
+              "& fieldset": { borderColor: "#4B0082" }, // Secondary color
+              color: "#4B0082",
+            },
+          },
+          "& .MuiInputLabel-root": {
+            color: "#4B0082", // Default text color
+          },
+          "& .MuiInputLabel-root.Mui-focused": {
+            color: "#ffd700", // Focused text color
+          },
+        },
+      },
+    },
+    MuiCheckbox: {
+      styleOverrides: {
+        root: {
+          color: "#ffd700", // Default color (secondary text color)
+          "&.Mui-checked": {
+            color: "#4B0082", // Checked color (secondary main color)
+          },
+        },
+      },
+    },
+  },
 });
 
 export default theme;
