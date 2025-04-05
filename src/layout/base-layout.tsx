c3fe5a744f33fba4b9175980fb951a6588c87253
rehan <mohdrehanali40@gmail.com>
2025-04-05 13:00:59 +0530

    feat: added the react query configuration

--- src/layout/base-layout.tsx
+++ src/layout/base-layout.tsx
@@ -4,15 +4,19 @@ import Footer from "./components/footer";
 import { Outlet } from "react-router-dom";
 import theme from "../theme/index";
 import { DialogProvider } from "../common/components/dialog/dialog-provider-conext";
+import { QueryClientProvider } from "react-query";
+import { queryClient } from "../config/react-query-config";
 
 const BaseLayout: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <DialogProvider key={"Dialog Provider"}>
-        <TopBar />
-        <Outlet />
-        <Footer />
-      </DialogProvider>
+      <QueryClientProvider client={queryClient}>
+        <DialogProvider key={"Dialog Provider"}>
+          <TopBar />
+          <Outlet />
+          <Footer />
+        </DialogProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 };
